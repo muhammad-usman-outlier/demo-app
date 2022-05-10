82f6eea172004e644090c11e9dc6848363b25c46
muhammad-usman-outlier <muhammad.usman@engineering.outlier.org>
2022-05-10 05:29:39 -0600

    feat: timout 15 mins delay

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable jest/valid-expect */
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+var foo = 1;
+beforeAll(function(done) {
+  setTimeout(function(){
+    foo = 2;
+    done();
+  }, 900000)
+});
+
+jest.setTimeout(900001)
+describe("Async setup", function(){
+
+  it("should have foo equal to 2.", function(done){
+    expect(foo).toEqual(2);
+    done();
+  });
+
+  it("should have foo not equal 3.", function(done){
+    expect(foo).not.toEqual(3);
+    done();
+  });
+
 });
