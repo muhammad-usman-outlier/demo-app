58354204f871f82519daddce67e5523f88fc52ce
muhammad-usman-outlier <muhammad.usman@engineering.outlier.org>
2022-05-17 04:47:00 -0600

    ...

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,10 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn sreact/i)
-  expect(linkElement).toBeInTheDocument()
+describe('test', () => {
+  test('renders learn react link', () => {
+    render(<App />)
+    const linkElement = screen.getByText(/learn sreact/i)
+    expect(linkElement).toBeInTheDocument()
+  })
 })
