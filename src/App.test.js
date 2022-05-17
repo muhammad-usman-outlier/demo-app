6e0107997a47e28b9116b15e168b9a39479eecbd
muhammad-usman-outlier <muhammad.usman@engineering.outlier.org>
2022-05-17 04:06:37 -0600

    failing test

--- src/App.test.js
+++ src/App.test.js
@@ -4,29 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/learn sreact/i)
   expect(linkElement).toBeInTheDocument()
 })
-
-var foo = 1;
-beforeAll(function(done) {
-  setTimeout(function(){
-    foo = 2;
-    done();
-  }, 900000)
-});
-
-jest.setTimeout(900001)
-describe("Async setup", function(){
-
-  it("should have foo equal to 2.", function(done){
-    expect(foo).toEqual(2);
-    done();
-  });
-
-  it("should have foo not equal 3.", function(done){
-    expect(foo).not.toEqual(3);
-    done();
-  });
-
-});
